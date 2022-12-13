7e710b20a1a46e624792099b766c44754e7f3533
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-12-13 11:06:47 -0600

    haciendo una integration test

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,39 @@
+const mockGetAll = jest.fn();
+
+const request = require('supertest');
+
+const { generateManyBook } = require('../src/fakes/book.fake');
+
+const createApp = require('../src/app');
+
+describe('test for books', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return list books', () => {
+      // Arrange
+      const fakeBooks = generateManyBook(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      return request(app)
+      // la diagonal al principio siempre va ahi, RECUERDALO.
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
+// todo esto solamente prueba que funcione correctamente la ruta, evitando
